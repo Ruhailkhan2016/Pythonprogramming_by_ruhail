7edd76844a043cfb83d41bb0359bcb8075c2be42
Ruhail Khan <ruhailkhan2016@gmail.com>
2022-10-06 19:50:38 +0530

    python code

---
+++ Python_Basics_Programs/swap.py
@@ -0,0 +1,17 @@
+a = int(input("Enter value of A: "))                #20
+b = int(input("Enter value of B: "))                #30
+print("A = {} and B = {}".format(a, b))
+
+
+a = a + b                                           # 20 + 30 = 50
+b = a - b                                           # 50 - 30 = 20
+a = a - b                                           # 50 - 20 = 30 
+
+
+print("Now, A = {} and B = {}".format(a, b))
+
+
+
+
+
+
